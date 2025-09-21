da1afe2f322de6c52c44395b7f1a8c87a558d105
didask014 <didask014@gmail.com>
2025-09-21 08:51:26 -0500

    Desafio terminado

--- app.js
+++ app.js
@@ -2,90 +2,94 @@
 
 let amigos = [];
 let listaAmigosHTML = etiquetaLista("listaAmigos");
+let amigoSorteado = etiquetaLista("resultado");
 
-
-function ingresarAmigos (nombre){
-    amigos.push(nombre);
-    return mostrarLista();
-}
-
-function mostrarLista(){
-    console.log(`La lista de amigos es: ${amigos}`);
-}
-
-function agregarAmigo(){
-    if (validarEntrada() === true) {
-        let cajaEntrada = document.getElementById("amigo").value;
-        ingresarAmigos(cajaEntrada);
-        enlazarListas();
-        borrarCajaInput();
-    } else {
-        alert(`Por favor, inserte un nombre`);
+// Primera parte
+    function ingresarAmigos (nombre){
+        amigos.push(nombre);
+        return mostrarLista();
     }
-}
 
-function borrarCajaInput(){
-    let cajaEntrada = document.querySelector("#amigo");
-    cajaEntrada.value = "";
-}
-
-function validarEntrada(){
-    if (cajaEntrada = document.getElementById("amigo").value === ""){
-        return false;           // retorna falso si no hay texto en la caja                                                      
-    } else {
-        return true;            // retorna verdadero si existe texto en la caja
+    function mostrarLista(){
+        console.log(`La lista de amigos es: ${amigos}`);
     }
-}
+
+    function agregarAmigo(){
+        if (validarEntrada() === true) {
+            let cajaEntrada = document.getElementById("amigo").value;
+            ingresarAmigos(cajaEntrada);
+            enlazarListas();
+            borrarCajaInput();
+        } else {
+            alert(`Por favor, inserte un nombre`);
+        }
+    }
+
+    function borrarCajaInput(){
+        let cajaEntrada = document.querySelector("#amigo");
+        cajaEntrada.value = "";
+    }
+
+    function validarEntrada(){
+        if (cajaEntrada = document.getElementById("amigo").value === ""){
+            return false;           // retorna falso si no hay texto en la caja                                                      
+        } else {
+            return true;            // retorna verdadero si existe texto en la caja
+        }
+    }
 
 // Codigo de las listas
 
-function etiquetaLista(id){
-    return document.getElementById(id);
-}
-
-function crearElementosLI (elemento){
-    let nuevoLI = document.createElement("li");
-    nuevoLI.textContent = elemento;
-    return nuevoLI;
-}
-
-function addtoListaHTML(lista, elemento){
-    lista.appendChild(crearElementosLI(elemento));
-}
-
-function enlazarListas (){
-    borrarAnteriorListaHTML(listaAmigosHTML);
-    for (let i = 0; i < amigos.length; i++ ){
-        addtoListaHTML(listaAmigosHTML ,amigos[i]);
+    function etiquetaLista(id){
+        return document.getElementById(id);
     }
-}
 
-function borrarAnteriorListaHTML (lista){
-    lista.innerHTML = "";
-}
+    function crearElementosLI (elemento){
+        let nuevoLI = document.createElement("li");
+        nuevoLI.textContent = elemento;
+        return nuevoLI;
+    }
+
+    // Ingresa la lista y el elemento que quieras ingresar a esa lista
+    function addtoListaHTML(lista, elemento){
+        lista.appendChild(crearElementosLI(elemento));
+    }
+
+    function enlazarListas (){
+        borrarAnteriorListaHTML(listaAmigosHTML);
+        for (let i = 0; i < amigos.length; i++ ){
+            addtoListaHTML(listaAmigosHTML ,amigos[i]);
+        }
+    }
+
+    function borrarAnteriorListaHTML (lista){
+        lista.innerHTML = "";
+    }
 
 // Generación del nombre sorteado
 
-function generarNumeroAleatorio (){
-    let indice = Math.floor(Math.random() * amigos.length);
-    return indice;
-}
-
-function comprobarNulidadAmigos(){
-    if (amigos.length === 0){
-        return false;               // retorna falso si no hay elementos
-    }else {
-        return true;                // retorna verdadero si existen elementos
+    function generarNumeroAleatorio (){
+        let indice = Math.floor(Math.random() * amigos.length);
+        return indice;
     }
-}
 
-function obtenerNombreSorteado(){
-    return amigos[generarNumeroAleatorio()]    
-}
-
-function sortearAmigo() {
-    if (comprobarNulidadAmigos()){
-
-        addtoListaHTML()
+    function comprobarNulidadAmigos(){
+        if (amigos.length === 0){
+            return false;               // retorna falso si no hay elementos
+        }else {
+            return true;                // retorna verdadero si existen elementos
+        }
     }
-}
+
+    function obtenerNombreSorteado(){
+        return amigos[generarNumeroAleatorio()]    
+    }
+
+    function sortearAmigo() {
+        if (comprobarNulidadAmigos()){
+            borrarAnteriorListaHTML(amigoSorteado);
+            addtoListaHTML(amigoSorteado, obtenerNombreSorteado());
+        }else{
+            alert(`Debes ingresar al menos un nombre para poder sortear`);
+        }
+    }
